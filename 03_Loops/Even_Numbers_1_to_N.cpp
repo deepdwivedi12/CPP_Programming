815d1f687cd875b93b4871edcb10b7c268851c24
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-06-23 11:33:54 +0530

    Create Even_Numbers_1_to_N.cpp

---
+++ 03_Loops/Even_Numbers_1_to_N.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the range to print a even number :"
+    cin >> n;
+
+    for(int i=2; i<=n; i+=2) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
